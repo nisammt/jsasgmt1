dafc51656e6c3d40fa64400d032320d5f7f91d18
nisam <nisammt@gmail.com>
2024-07-19 11:00:27 +0530

    Project add

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Javasript Assaignmen1")
+
+let a ;
+let i ;
+
+function getEvenNums(a)
+{
+    for(i=0; i<=a; i++)
+      if(i % 2 == 0)
+      {
+        console.log(i);
+      }
+
+}
+getEvenNums(11);
